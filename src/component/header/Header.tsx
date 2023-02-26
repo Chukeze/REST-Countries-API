46a76513c64d5036f9879bf73adbdf0a2c856edf
Chukeze <enile12@gmail.com>
2023-02-26 14:42:47 -0600

    Add header class name using BEM. Also  add linting

---
+++ src/component/header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <h1>Where in the world?</h1>
+        <div className="color-theme-container">
+            <span>moon</span>
+            <h2 className='color-theme-container__color'>Dark Mode</h2>
+        </div>
+    </header>
+  )
+}
+
+export default Header
