67f2f474b0f136a99f81f5ca9e94e12ea88db4e8
Chukeze <enile12@gmail.com>
2023-02-26 21:45:58 -0600

    Scaffold and add preliminary style for cards

---
+++ src/component/cards/Card.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='card'>
+        <div className="countryFlag">
+            
+        </div>
+        <p className="countryName"></p>
+        <p className="population">Population: </p>
+        <p className="region">Region:</p>
+        <p className="capital">Capital:</p>
+    </div>
+  )
+}
+
+export default Card
