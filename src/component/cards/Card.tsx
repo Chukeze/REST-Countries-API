1006587ac9cbc3a65af32ae07b400f1bdc9bd3d7
Chukeze <enile12@gmail.com>
2023-02-28 14:17:52 -0600

    Fix style and rendering of the cards

--- src/component/cards/Card.tsx
+++ src/component/cards/Card.tsx
@@ -3,27 +3,24 @@ import './../cards/Card.css'
 import data from '../../assets/data/default/data.json'
 
 const Card = () => {
-
-  return (
-    <div className='card'>
-      { data && 
-          data.map( (item) => {
-            return (
-              //pass the data to the prop of the country detail component 
-              <CountryDetails
-                key={item.name}
-                flag={item.flags.svg}
-                name={item.name}
-                population={item.population}
-                region={item.region}
-                capital={item.capital}
-              />
-            );
-          })
-      }
-      
-    </div>
-  )
+  return(
+    <>
+      {data && data.map( (item) =>{
+      return(
+        <div className='card' key={item.nativeName}>
+          <CountryDetails
+            key={item.name}
+            flag={item.flags.svg}
+            name={item.name}
+            population={item.population}
+            region={item.region}
+            capital={item.capital}
+          />
+        </div>
+      );
+      })}
+    </>
+  );
 }
 
 export default Card
--- src/component/countryDetails/CountryDetails.tsx
+++ src/component/countryDetails/CountryDetails.tsx
@@ -25,24 +25,32 @@ const CountryDetails: FC<Props> = ({
         language,
 }) => {
 //{{//I assign the values of props to a specific html element}}
-  return (
-    <>
-        <div className="countryFlag">
-            <img src={flag} alt={name} className="countryFlag__image" />
-        </div>
-        <div className="countryInfo">
-            <p className="countryInfo--name">{name}</p>            
-            <p className="countryInfo--text">Population: <span className='country-info--value'>{population} </span></p>
-            <p className="countryInfo--text">Region: <span className='country-info--value'>{region}</span></p>
-            <p className="countryInfo--text">Capital: <span className='country-info--value'>{capital}</span></p> 
-            <p className="countryInfo--text">Sub Region: <span className='country-info--value'>{subregion}</span></p>
-            <p className="countryInfo--text">Top Level Domain: <span className='country-info--value'>{domain}</span></p>
-            <p className="countryInfo--text">Currencies: <span className='country-info--value'>{currency}</span></p>  
-            <p className="countryInfo--text">Languages: <span className='country-info--value'>{language}</span></p>  
-        </div>
-    </>
-    
-  )
+        return (
+            <>
+                <div className="countryFlag">
+                    <img src={flag} alt={name} className="countryFlag__image" />
+                </div>
+                <div className="countryInfo">
+                    {domain || currency || subregion || language === (null || undefined) ?(
+                        <><p className="countryInfo--name">{name}</p>            
+                        <p className="countryInfo--text">Population: <span className='country-info--value'>{population} </span></p>
+                        <p className="countryInfo--text">Region: <span className='country-info--value'>{region}</span></p>
+                        <p className="countryInfo--text">Capital: <span className='country-info--value'>{capital}</span></p></>
+                    ) : (
+                        <>
+                            <p className="countryInfo--name">{name}</p>            
+                            <p className="countryInfo--text">Population: <span className='country-info--value'>{population} </span></p>
+                            <p className="countryInfo--text">Region: <span className='country-info--value'>{region}</span></p>
+                            <p className="countryInfo--text">Capital: <span className='country-info--value'>{capital}</span></p> 
+                            <p className="countryInfo--text">Sub Region: <span className='country-info--value'>{subregion}</span></p>
+                            <p className="countryInfo--text">Top Level Domain: <span className='country-info--value'>{domain}</span></p>
+                            <p className="countryInfo--text">Currencies: <span className='country-info--value'>{currency}</span></p>  
+                            <p className="countryInfo--text">Languages: <span className='country-info--value'>{language}</span></p>
+                        </>
+                    )}
+                </div>
+            </>
+        )
 }
 
 export default CountryDetails
